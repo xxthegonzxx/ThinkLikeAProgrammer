fbb3e67c16d555c660cd90355823b6291e8a9eb5
Gonzo Fernandez <xxthegonzxx@gmail.com>
2022-08-25 12:32:46 -0700

    Exercise 4-1 completed
    This challenge uses LinkedLists to add, delete, and traverse the list.

---
+++ Chapter 4/Exercise_4-1/README.md
@@ -0,0 +1,5 @@
+# Exercise 4-1
+
+Design your own: Take a problem that you already know how to solve using an array but that is limited by the size of the array. Rewrite the code to remove that limitation using a dynamically allocated array.
+
+**Note:** *Since go already has dynamically allocated slices this challenge will instead focus on working with linked lists.*
---
+++ Chapter 4/Exercise_4-1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type listNode struct {
+	grade     int
+	studentID int
+	name      string
+	next      *listNode
+}
+
+type studentCollection struct {
+	head *listNode
+}
+
+func (sc *studentCollection) addRecord(n *listNode) {
+	newNode := listNode{
+		grade:     n.grade,
+		studentID: n.studentID,
+		name:      n.name,
+		next:      sc.head,
+	}
+	sc.head = &newNode
+}
+
+func (sc *studentCollection) deleteRecord(name string) {
+	student := sc.head
+	for student != nil {
+		if student.name != name {
+			fmt.Printf("%d %d %s\n", student.grade, student.studentID, student.name)
+		}
+		student = student.next
+	}
+}
+
+func (sc studentCollection) traverseList() {
+	student := sc.head
+	for student != nil {
+		fmt.Printf("%d %d %s\n", student.grade, student.studentID, student.name)
+		student = student.next
+	}
+}
+
+func main() {
+	studentList := studentCollection{}
+	students := []listNode{
+		{grade: 68, studentID: 1002, name: "Freddie"},
+		{grade: 100, studentID: 1003, name: "Gonzalo"},
+		{grade: 48, studentID: 1001, name: "Eric"},
+		{grade: 78, studentID: 1004, name: "Sandra"},
+		{grade: 98, studentID: 1005, name: "Irina"},
+	}
+	sort.Slice(students, func(i, j int) bool {
+		return students[i].grade < students[j].grade
+	})
+	for student := range students {
+		studentList.addRecord(&students[student])
+	}
+	fmt.Println("By Grade:")
+	studentList.traverseList()
+	fmt.Println("-----")
+	studentList.deleteRecord("Eric")
+}
