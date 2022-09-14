fda3f115ff1d741f1d3cdeff9d3d5cee7b21f838
Gonzo Fernandez <xxthegonzxx@gmail.com>
2022-09-14 13:10:24 -0700

    Exercise 6-10 complete

---
+++ Chapter6/Exercise_6-10/README.md
@@ -0,0 +1,3 @@
+# Exercise 6-10
+
+A *binary search tree* is a binary tree in which each node value is greater than any value in that node's left subtree but less than any value in the node's right subtree. Write a recrusive functionto determine whether a binary tree is a binary search tree.
---
+++ Chapter6/Exercise_6-10/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type treeNode struct {
+	data  int
+	left  *treeNode
+	right *treeNode
+}
+
+type treePtr struct {
+	head *treeNode
+}
+
+func recurseBinarySearchTree(n *treeNode) bool {
+	if n == nil {
+		return false
+	}
+	if n.left == nil && n.right == nil {
+		return false
+	}
+	recurseBinarySearchTree(n.left)
+	recurseBinarySearchTree(n.right)
+	if n.data > n.left.data && n.data < n.right.data {
+		return true
+	}
+	return false
+}
+
+func main() {
+	node7 := &treeNode{4000, nil, nil}
+	node6 := &treeNode{50, nil, nil}
+	node5 := &treeNode{600, nil, nil}
+	node4 := &treeNode{70, nil, nil}
+	node3 := &treeNode{800, node6, node7}
+	node2 := &treeNode{90, node4, node5}
+	rootNode := &treeNode{100, node2, node3}
+
+	toPrint := recurseBinarySearchTree(rootNode)
+	fmt.Println(toPrint)
+}
